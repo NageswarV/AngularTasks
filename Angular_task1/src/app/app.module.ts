6e9e4be141db6e6d498055897df82cf777cee642
NageswarV <vnrakki111@gmail.com>
2020-04-21 11:39:29 +0530

    initial commit

---
+++ Angular_task1/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ComponentOneComponent } from './component-one/component-one.component';
+import { ComponentTwoComponent } from './component-two/component-two.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ComponentOneComponent,
+    ComponentTwoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
