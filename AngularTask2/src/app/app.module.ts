b267a150ebae208b5b1f5ff62814ca0a45ae31b4
NageswarV <vnrakki111@gmail.com>
2020-04-22 13:22:34 +0530

    second commit

--- AngularTask2/src/app/app.module.ts
+++ AngularTask2/src/app/app.module.ts
@@ -1,26 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { UserService } from './user.service';
 import { ActiveModule } from './active/active.module';
-import { ManageModule } from './manage/manage.module';
 import { DeletedModule } from './deleted/deleted.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent
-  ],
-  imports: [
-    BrowserModule,
-    ActiveModule,
-    DeletedModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HomeComponent],
+  imports: [BrowserModule, ActiveModule, DeletedModule, AppRoutingModule],
   providers: [UserService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
